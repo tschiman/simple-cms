1ee16f34fbb5ae639a5dc2bf5cacd84a32424dd3
Tim Schimandle <tschiman@gmail.com>
2024-07-15 10:16:02 -0600

    Update validation on main page edit and change the title of the index page

--- src/main/java/com/gettimhired/simplecms/model/dto/MainPageEditDTO.java
+++ src/main/java/com/gettimhired/simplecms/model/dto/MainPageEditDTO.java
@@ -1,17 +1,33 @@
 package com.gettimhired.simplecms.model.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.web.multipart.MultipartFile;
 
 public record MainPageEditDTO(
+        @NotBlank
+        @Size(min=1, max = 256, message = "Title must be between 1 and 256 characters")
         String title,
         Boolean hasMainPageImage,
         MultipartFile mainPageImage,
         Boolean deleteMainPageImage,
+        @NotBlank
+        @Size(min=1, max = 256, message = "Section 1 Title must be between 1 and 256 characters")
         String sectionOneTitle,
+        @NotBlank
+        @Size(min=1, max = 1000, message = "Section 1 Content must be between 1 and 1000 characters")
         String sectionOneContent,
+        @NotBlank
+        @Size(min=1, max = 256, message = "Section 2 Title must be between 1 and 256 characters")
         String sectionTwoTitle,
+        @NotBlank
+        @Size(min=1, max = 1000, message = "Section 2 Content must be between 1 and 1000 characters")
         String sectionTwoContent,
+        @NotBlank
+        @Size(min=1, max = 256, message = "Section 3 Title must be between 1 and 256 characters")
         String sectionThreeTitle,
+        @NotBlank
+        @Size(min=1, max = 1000, message = "Section 3 Content must be between 1 and 1000 characters")
         String sectionThreeContent
 ) {
 }
